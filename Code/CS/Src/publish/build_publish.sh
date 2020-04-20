e82d6e0d501a0beac86a489c9ea86a381a13865f
zhaojie02 <zhaojie02@sobey.com>
2020-04-20 16:19:17 +0800

    Description：内容管理微服务改造
    Signed-off-by: zhaojie02 <zhaojie02@sobey.com>

---
+++ Code/CS/Src/publish/build_publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -il
+# author: zj 
+
+. /etc/bashrc
+
+bin=$(cd $(dirname $0); pwd)
+echo cd $bin
+cd $bin
+
+rm -rf app/* 
+
+cd ../
+
+# cd CNTM.WebApi/
+dotnet publish CCETest.csproj -c Release -o ../publish/app
+
+if [ $? -ne 0 ] ; then
+    echo "dotnet build error"
+    exit 1
+fi
+
+cd ../publish
+
+docker build -t ccetest:1.0 .
+
+if [ $? -ne 0 ] ; then
+    echo "docker build error"
+    exit 1
+fi
+
+rm -rf app/*
+
+

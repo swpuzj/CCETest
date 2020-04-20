36ebae45f001889988463e7a17257e5dab8b48f4
zhaojie02 <zhaojie02@sobey.com>
2020-04-20 18:46:15 +0800

    xxxx1

--- Code/CS/Src/publish/build_publish.sh
+++ Code/CS/Src/publish/build_publish.sh
@@ -11,8 +11,7 @@ rm -rf app/*
 
 cd ../
 
-# cd CNTM.WebApi/
-dotnet publish CCETest.csproj -c Release -o ../publish/app
+dotnet publish CCETest.csproj -c Release -o publish/app
 
 if [ $? -ne 0 ] ; then
     echo "dotnet build error"
